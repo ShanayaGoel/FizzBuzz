ef1f087183a6d4dff6560b78e6738369d749df2a
Shanaya Goel <shana2@MacBook-Air-4.local>
2024-09-11 23:30:23 -0400

    coded Reduce class

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,16 @@
 public class Reduce {
+    public static void main(String[] args) {
+        int count = 0;
+        int num = 100;
+        while( num != 0){
+            if (num%2 == 0){
+                num /= 2;
+            }
+            else{
+                num -= 1;
+            }
+            count++;
+        }
+        System.out.printf(String.valueOf(count));
+    }
 }
